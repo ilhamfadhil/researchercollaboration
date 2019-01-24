2f4ca0abf06b39f46d310a98934210cfaa933774
ilhamfadhil <ilhamfadhil@gmail.com>
2019-01-24 18:09:06 +0700

    Update clean_network.R

--- clean_network.R
+++ clean_network.R
@@ -74,3 +74,12 @@ network <- graph_from_data_frame(aut_condata_clean[, 1:2], directed = FALSE)
 
 V(network)$size <- 2
 plot.igraph(network, layout = layout.fruchterman.reingold, vertex.label = NA)
+
+png("network-basic.png",
+    width  = 3.25,
+    height = 3.25,
+    units  = "in",
+    res    = 1200,
+    pointsize = 4)
+plot.igraph(network, layout = layout.fruchterman.reingold, vertex.label = NA)
+dev.off()
